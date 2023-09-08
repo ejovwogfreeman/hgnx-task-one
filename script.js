6d55d27a28d4b3471eed20932bb03d55f3736186
ejovwogfreeman <ejovwogfreeman007@gmail.com>
2023-09-08 15:28:33 +0100

    message

---
+++ script.js
@@ -0,0 +1,30 @@
+function updateDateTime() {
+  const daysOfWeek = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  const currentDate = new Date();
+
+  const currentDayOfTheWeekElement = document.getElementById("dayOfTheWeek");
+  const currentUTCtimeElement = document.getElementById("utcTime");
+
+  const currentDay = daysOfWeek[currentDate.getUTCDay()];
+  currentDayOfTheWeekElement.textContent = currentDay;
+
+  const currentUtcTime = currentDate.toLocaleTimeString("en-US", {
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    timeZoneName: "short",
+  });
+  currentUTCtimeElement.textContent = currentUtcTime;
+}
+
+updateDateTime();
+setInterval(updateDateTime, 1000);
